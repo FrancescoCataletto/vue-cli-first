24730979cf9c5ed42b6f5c0a9528dcd154233f73
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-12 17:43:24 +0200

    added jumbotron and centered elements

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
     <titleElement />
+
+    <HeroElement />
   </div>
 </template>
 
 <script>
 import titleElement from "./components/titleElement.vue"
+import HeroElement from "./components/HeroElement.vue"
 
 export default {
   name: 'App',
   components: {
-    titleElement
-  }
+    titleElement,
+    HeroElement
+}
 }
 </script>
 
 <style>
-
+  #app{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
---
+++ src/components/HeroElement.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbo">Questo è il Jumbotron della mia prima app con Vue CLI</div>
+</template>
+
+<script>
+export default {
+    name: "HeroElement"
+}
+</script>
+
+<style scoped>
+.jumbo{
+    height: 500px;
+    width: 500px;
+    background-color: antiquewhite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
