2d80941cd31c5db14047f99eb6efb0cf2dd8d472
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-12 17:37:20 +0200

    printed title in page using VueCLI

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <titleElement />
   </div>
 </template>
 
 <script>
-
+import titleElement from "./components/titleElement.vue"
 
 export default {
   name: 'App',
   components: {
-    
+    titleElement
   }
 }
 </script>
